91a279dfab487e4660ba816b8a0d73f8c3dec85d
David Navarro Alvarez <davengeo@yahoo.es>
2015-07-21 10:36:19 +0200

    Initial commit

---
+++ src/learning_two/core.clj
@@ -0,0 +1,3 @@
+(ns learning-two.core)
+
+
---
+++ test/learning_two/core_test.clj
@@ -0,0 +1,7 @@
+(ns learning-two.core-test
+  (:require [clojure.test :refer :all]
+            [learning-two.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
