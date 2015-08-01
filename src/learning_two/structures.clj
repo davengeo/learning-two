4456d3061ab31be1c149085079e9cf18edb8cd1f
David Navarro Alvarez <davengeo@yahoo.es>
2015-08-01 19:17:41 +0200

    starting some experiments about persistent structures

--- src/learning_two/structures.clj
+++ src/learning_two/structures.clj
@@ -1,7 +1,16 @@
-(ns learning-two.structures)
+(ns learning-two.structures
+  (:use midje.sweet))
 
 
-(def baselist (list :barnabas :adam))
+(def baselist (list :pedro :miki))
 
-(def lst1 (cons :willie baselist))
-(def lst2 (cons :phoenix baselist))
+(def lst1 (cons :monica baselist))
+(def lst2 (cons :david baselist))
+
+(fact "they both are composed of baselist"
+      (= (next lst1) (next lst2)) => true)
+
+(fact "they both are identical!!!"
+      (identical? (next lst1) (next lst2)) => true)
+
+(def my-tree {:val 50, :L nil, :R nil})
